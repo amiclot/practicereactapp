c5bd3dff38b9cfe503b433d0af168a1a407898fa
Adam Miclot <amiclot09@gmail.com>
2017-11-30 14:20:32 -0500

    add css second way

--- src/App.js
+++ src/App.js
@@ -35,11 +35,21 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1x solid blue',
+      padding: '8px'
+    };
+
     return (
       <div className="App">
         <h1>Hi, blah blah</h1>
         <p>This works</p>
-        <button onClick={() => this.switchNameHandler('Bling Blow')}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Bling Blow')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}/>
