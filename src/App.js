673ecc4ed4b3b911697f29f45f60118a00416a73
Adam Miclot <amiclot09@gmail.com>
2017-11-30 14:04:12 -0500

    input

--- src/App.js
+++ src/App.js
@@ -12,32 +12,43 @@ class App extends Component {
     otherState: 'some other shit'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('was clicked');
     // this.state.persons[0].name = 'Bum'; (DONT DO THIS)
     this.setState({
       persons: [
-      {name: 'Doo', age: 28},
+      {name: newName, age: 28},
       {name: 'Bruh', age: 88},
       {name: 'Cita', age: 1}
     ]
   })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+      {name: "Max", age: 28},
+      {name: event.target.value, age: 88},
+      {name: 'Cita', age: 1}
+    ]
+  })
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Hi, blah blah</h1>
         <p>This works</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Bling Blow')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}/>
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler}>My Hobbies: Racing</Person>
+          //bind is better for performance
+          click={this.switchNameHandler.bind(this, 'Crumb')}
+          changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
 
         <Person 
           name={this.state.persons[2].name} 
