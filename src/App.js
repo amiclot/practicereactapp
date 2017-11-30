e7fd20e18fec993ec60b454308a508194a3d1d8c
Adam Miclot <amiclot09@gmail.com>
2017-11-30 15:00:55 -0500

    render through JSX, better

--- src/App.js
+++ src/App.js
@@ -55,15 +55,10 @@ class App extends Component {
       cursor: 'pointer'
     };
 
-    return (
-      <div className="App">
-        <h1>Hi, blah blah</h1>
-        <p>This works</p>
-        <button 
-        style={style}
-        onClick={this.togglePersonsHandler}>Switch Name</button>
-        
-        {this.state.showPersons === true ? 
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (
           <div>
             <Person 
               name={this.state.persons[0].name} 
@@ -78,8 +73,21 @@ class App extends Component {
             <Person 
               name={this.state.persons[2].name} 
               age={this.state.persons[2].age}/>
-          </div> : null
-      }
+          </div>
+        );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, blah blah</h1>
+        <p>This works</p>
+        <button 
+        style={style}
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+        
+          
+
 
       </div>
     );
